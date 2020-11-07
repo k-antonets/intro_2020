c14c0c8bb5cebf3f52ab2d11de174a799291e452
kirant <k.antonets@arriam.ru>
2020-11-07 12:16:45 +0300

    new messages for bases input

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from mymath.int_func import my_int as mi, int_to_str as i2s
 if __name__ == '__main__':
     n1 = input('Please, type number 1: ')  # n1='12af'
     n2 = input('Please, type number 2: ')  # n2='3b'
-    input_base = mi(input('Input base: '))
-    output_base = mi(input('Output base: '))
+    input_base = mi(input('Please, type Input base: '))
+    output_base = mi(input('Please, type Output base: '))
 
     ni1 = mi(n1, input_base)
     ni2 = mi(n2, input_base)
