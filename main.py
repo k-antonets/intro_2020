e9aa1b0d66e77b469f49e79d45f4c3b08b018d0c
kirant <k.antonets@arriam.ru>
2020-11-07 11:28:46 +0300

    add new line to output

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from math.int_func import my_int as mi, int_to_str as i2s
 
 if __name__ == '__main__':
-    n1 = input('number 1: ')
-    n2 = input('number 2: ')
+    n1 = input('number 1: ')  # n1='12af'
+    n2 = input('number 2: ')  # n2='3b'
     input_base = mi(input('Input base: '))
     output_base = mi(input('Output base: '))
 
@@ -11,4 +11,5 @@ if __name__ == '__main__':
 
     res = i2s(ni1 + ni2, output_base)
 
+    print('Result for "' + n1 + '+' + n2 + '":')  # Result for "12af+3b":
     print(res)
